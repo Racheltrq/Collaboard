d393aeb1f260b298013bce12bd6c75a742b0c01d
Rachel Tao <rt1726@nyu.edu>
2019-06-11 07:16:23 +0800

    css not showing
    It works on online editor but does not work on my local server.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Login from "./login";
 
-ReactDOM.render(<div><h1>Welcome!</h1></div>, document.getElementById("root"));
+
+ReactDOM.render(<Login />, document.getElementById("root"));
+
---
+++ my-app/src/login.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Login(){
+	return(
+		<div>
+			<h1 className = "header"> Welcome!</h1>
+
+			<div className = "login" style = {{float: 'left'}}>
+				Login:
+				<form>
+				  <label>
+				    Username:
+				    <input type="text" name="usr" />
+				  </label>
+				</form>
+
+				<form>
+				  <label>
+				    Password:
+				    <input type="text" name="psw" />
+				  </label>
+				  <input type="submit" value="Submit" />
+				</form>
+			</div>
+
+			<div className = "login">
+				Sign up:
+				<form>
+				  <label>
+				    Username:
+				    <input type="text" name="usr" />
+				  </label>
+				</form>
+
+				<form>
+				  <label>
+				    Password:
+				    <input type="text" name="psw" />
+				  </label>
+				  <input type="submit" value="Submit" />
+				</form>
+			</div>
+
+
+		</div>
+	)
+}
+
+
+export default Login
