db2e40abfd6449fd0d63832040691b1e42f13f44
Rachel Tao <rt1726@nyu.edu>
2019-06-20 16:22:08 +0800

    add redirect to file page

---
+++ my-app/src/file.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class File extends React.Component{
+	render(){
+		return(
+			<div style = {{height: 100}}>
+
+			</div>
+
+			)
+	}
+}
+
+export default File
--- my-app/src/grid.js
+++ my-app/src/grid.js
@@ -4,10 +4,12 @@ import React from "react"
 class Grid extends React.Component{
 	render(){
 		return(
-			<div style = {{width: 206, height: 240, marginLeft: 30, marginBottom: 25, backgroundColor: "#EEDBD7", borderRadius: 3}}>
-				<img src = {this.props.pic} class="rounded mx-auto d-block" style = {{width: 200, height: 200, margin: 4}}/>
-				<p>Baby dragon image</p>
-			</div>
+			<a href = "/file" style={{ textDecoration: 'none', color: 'black' }}>
+				<div style = {{width: 206, height: 240, marginLeft: 30, marginBottom: 25, backgroundColor: "#EEDBD7", borderRadius: 3}}>
+					<img src = {this.props.pic} class="rounded mx-auto d-block" style = {{width: 200, height: 200, margin: 4}}/>
+					<p>Baby dragon image</p>
+				</div>
+			</a>
 			)
 	}
 }
