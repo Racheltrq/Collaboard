300b67d9baef5817a4ce8675bdd5580848151ef2
Rachel Tao <rt1726@nyu.edu>
2019-07-10 10:50:59 +0800

    move css to file.css

--- my-app/src/file.js
+++ my-app/src/file.js
@@ -10,10 +10,10 @@ class File extends React.Component{
 	render(){
 		return(
 			<div>
-				<div style = {{height: 70, backgroundColor: 'white'}}>
-					<h3 style= {{marginLeft: 15}}>Filename</h3>
+				<div id = 'filenameDiv'>
+					<h3 id = 'filename'>Filename</h3>
 				</div>
-				<SketchField width='1024px' height='680px' tool={Tools.Pencil} lineColor='black' lineWidth={3} style = {{margin: 10, backgroundColor: "white"}}/>
+				<SketchField width='1024px' height='680px' tool={Tools.Pencil} lineColor='black' lineWidth={3}  id = 'sketchField'/>
 				<Chat />
 			</div>
 			)
