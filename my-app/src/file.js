f9179bbaa1f4ac8c8fa776ea8ef56939801efa83
Rachel Tao <rt1726@nyu.edu>
2019-07-10 15:33:54 +0800

    move css to file.css

--- my-app/src/file.js
+++ my-app/src/file.js
@@ -1,9 +1,9 @@
 import React from "react"
 import {SketchField, Tools} from 'react-sketch';
-
+import './css/file.css'
 
 import Chat from './chat'
-import './css/file.css'
+import 'react-sliding-pane/dist/react-sliding-pane.css';
 
 class File extends React.Component{
 	
@@ -12,10 +12,9 @@ class File extends React.Component{
 		return(
 			<div>
 				<div id = 'filenameDiv'>
-					<h3 id = 'filenameHeader'>Filename</h3>
+					<h3 id = 'filename'>Filename</h3>
 				</div>
-				<div id = 'spaceDiv'/>
-				<SketchField width='1024px' height='680px' tool={Tools.Pencil} lineColor='black' lineWidth={3} backgroundColor = 'white'/>
+				<SketchField width='1024px' height='680px' tool={Tools.Pencil} lineColor='black' lineWidth={3}  id = 'sketchField'/>
 				<Chat />
 			</div>
 			)
