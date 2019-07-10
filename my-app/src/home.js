ba60131b523f7b8b60ed8f97e6a429b359f0d1c2
Rachel Tao <rt1726@nyu.edu>
2019-07-10 21:48:54 +0800

    remove text

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -17,7 +17,6 @@ class Home extends React.Component{
 	render(){
 		return(
 			<div>
-				Welcome to Home page~
 				<nav class="navbar navbar-expand-lg navbar-light bg-light">
 				  <a class="navbar-brand" href="/">Collaboard</a>
 				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
