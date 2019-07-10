27474ad3103e95a2234122d49c701adf6cedf234
Rachel Tao <rt1726@nyu.edu>
2019-07-10 15:33:24 +0800

    move css to history.css"=

--- my-app/src/history.js
+++ my-app/src/history.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Popup from "reactjs-popup";
+import './css/history.css'
 
 
 class History extends React.Component{
 	render(){
 		return(
 			<div>
-				  <Popup trigger={<div className = "button" style = {{border: "solid", width: 45, height: 45}}><h2 style = {{font: 10, textAlign: 'center'}}>H</h2></div>} modal>
+				  <Popup trigger={<div className = "button" id = 'historyButton'><h2 id = 'text-H'>H</h2></div>} modal>
 				    
 				  	<div>
 				  		<h4>History</h4>
