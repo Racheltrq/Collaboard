9038b64d0d894edfcb30b0b67eb4b6e80674b312
Rachel Tao <rt1726@nyu.edu>
2019-07-14 17:29:48 +0800

    fix navbar

--- my-app/src/home.js
+++ my-app/src/home.js
@@ -19,15 +19,13 @@ class Home extends React.Component{
 			<div>
 				<nav class="navbar navbar-expand-lg navbar-light bg-light">
 				  <a class="navbar-brand" href="/">Collaboard</a>
-				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				    <span class="navbar-toggler-icon"></span>
-				  </button>
 
-				  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+				  <div class = "container-fluid" id="navbarSupportedContent">
 				    <ul class="navbar-nav mr-auto">
 
 				      <li class="nav-item active">
-				        <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+				        <a class="nav-link" href="/home" id = "navBarHome">Home <span class="sr-only">(current)</span></a>
 				      </li>
 
 				      <Dropdown title = "New" content = "none"/>
