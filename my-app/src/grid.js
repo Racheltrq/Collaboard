ee722fafab7e07dd99158629564bbb6575f1e4b9
Rachel Tao <rt1726@nyu.edu>
2019-07-15 13:59:07 +0800

    add alt for images

--- my-app/src/grid.js
+++ my-app/src/grid.js
@@ -7,7 +7,7 @@ class Grid extends React.Component{
 		return(
 			<a href = "/file" id = 'text'>
 				<div id = 'fileDiv'>
-					<img src = {this.props.pic} class="rounded mx-auto d-block" id = 'img'/>
+					<img src = {this.props.pic} alt = "file" class="rounded mx-auto d-block" id = 'img'/>
 					<p>Baby dragon image</p>
 				</div>
 			</a>
