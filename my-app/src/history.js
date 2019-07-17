88942c4e79b2682a7ec98b2317b09c75c67db3d6
Rachel Tao <rt1726@nyu.edu>
2019-07-17 10:22:38 +0800

    apply popup changes to History

--- my-app/src/history.js
+++ my-app/src/history.js
@@ -1,22 +1,37 @@
 import React from "react";
-import Popup from "reactjs-popup";
 import './css/history.css'
-
+import Modal from "react-responsive-modal";
 
 class History extends React.Component{
-	render(){
-		return(
-			<div>
-				  <Popup trigger={<div className = "button" id = 'historyButton'><h2 id = 'text-H'>H</h2></div>} modal>
-				    
-				  	<div>
-				  		<h4>History</h4>
-				  	</div>
-				  </Popup>
-			</div>
+	state = {
+		open: false
+	};
 
-			)
+	onOpenModal = () => {
+		this.setState({ open: true });
+	};
+
+	onCloseModal = () => {
+		this.setState({ open: false });
+	};
+
+	render() {
+		const { open } = this.state;
+		return (
+			<div>
+				<a id = "HistoryButton" onClick={this.onOpenModal}><h2 id = 'text-H'>H</h2></a>
+				<Modal open={open} onClose={this.onCloseModal} center>
+					<div id = "HistoryModal">
+						<h4>History</h4>
+						<button className="close" onClick={this.state.modalIsOpen = false}/>
+
+					</div>
+				</Modal>
+			</div>
+		);
 	}
 }
+
+
 
 export default History
